667dffb7a4335054f426098bd7deb73e7318b46e
Vanessa Chu <10492258+vchu22@users.noreply.github.com>
2018-08-23 09:06:11 -0400

    grouped modules into bioseq package

--- use-dnautil.py
+++ use-dnautil.py
@@ -1,6 +1,6 @@
-from dnautil import *
+from bioseq import dnautil
 dnaseq ='acatagagcgnn'
-print "Your GC-content takes %f%% of the sequence"%gc(dnaseq)
-print "Does your sequence has a stop codon? ",("No","Yes")[has_stop_codon(dnaseq,0)]
-print "Does your sequence contain a stop codon counting from the 2nd position? ",("No","Yes")[has_stop_codon(dnaseq,frame=1)]
-print "The reverse complement of your sequence is '%s'"%reversecomplement(dnaseq)
+print "Your GC-content takes %f%% of the sequence"%dnautil.gc(dnaseq)
+print "Does your sequence has a stop codon? ",("No","Yes")[dnautil.has_stop_codon(dnaseq,0)]
+print "Does your sequence contain a stop codon counting from the 2nd position? ",("No","Yes")[dnautil.has_stop_codon(dnaseq,frame=1)]
+print "The reverse complement of your sequence is '%s'"%dnautil.reversecomplement(dnaseq)
